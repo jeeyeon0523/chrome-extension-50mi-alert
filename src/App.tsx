386a533525be3caf0e6d4d30d5e923c0867d1c30
Jin <jeeyeon0523@gmail.com>
2021-10-09 01:02:29 +0900

    add 50mi counter

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import 'moment-timezone';
+import moment from 'moment';
+import Moment from 'react-moment';
+import { useInterval } from 'react-use';
+
 
 function App() {
+
+  const NextBreakTime = () => {
+    const [now, setNow] = useState<string>();
+    const [breakTime, setBreakTime] = useState<Date>();
+    
+    // useInterval
+    useInterval(() => {
+      setNow(moment().format('YYYYMMDDhhmmss'));
+      const nowTime = moment().format('mmss');
+      if (nowTime === '5000'){
+        let m = moment().add(1,'hour').format('YYYY-MM-DD HH:mm');
+        setBreakTime(new Date(m));
+      }
+    }, 1000);
+    
+    const dateFormatting = (d: string):string => {
+      const date = d.substr(1);
+      return date;
+    }
+
+    useEffect(()=> {
+      const minutes = moment().format('mm');
+      if (Number(minutes)>= 50){
+        let m = moment().add(1,'hour').format('YYYY-MM-DD HH:50');
+        setBreakTime(new Date(m));
+      }else{
+        let m = moment().format('YYYY-MM-DD HH:50');
+        setBreakTime(new Date(m));
+      }
+     
+    },[])
+
+    return (
+      <>
+        <Moment durationFromNow format={'mm분 ss초 남음'} date={breakTime} filter={dateFormatting}></Moment>
+      </>
+    )
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='timer'>
+      <div className='Neon-text'>
+        다음 쉬는 시간까지
+        <div>
+          {NextBreakTime()}
+        </div>
+      </div>
     </div>
   );
 }
